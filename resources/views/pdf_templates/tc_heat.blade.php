2dc7a33481358a22fbb037991b2370c46ff07e09
Zhanbota <nurseit_sharip@mail.ru>
2018-03-26 15:00:24 +0600

    pdf файлы

--- resources/views/pdf_templates/tc_heat.blade.php
+++ resources/views/pdf_templates/tc_heat.blade.php
@@ -25,128 +25,257 @@
         {
             position: relative;
         }
+        .wrap{
+            width: 700px;
+            margin:0 auto;
+            overflow: hidden;
+            /*border:solid 1px #ccc;*/
+        }
+        .logo{
+            width:800px;
+            /*border:solid 1px #000;*/
+            text-align: center;
+            margin-bottom:10px;
+        }
+        .divider{
+            border-bottom: solid 2px #000;
+            width:600px;
+            margin:0 auto;
+        }
+        .small_text table td{
+            font-size: 9px;
+            padding-left: 95px;
+            font-family: "Times New Roman";
+        }
+
+        .number{
+            width:250px;
+            float: left;
+        }
+        .number p{
+            font-size: 12px;
+            text-align: center;
+            margin-left: 20px;
+
+        }
+        .number .underline{
+            text-decoration: underline;
+            margin-left: 22px;
+            font-size: 12px;
+        }
+        .number .third{
+            margin-left: 22px;
+        }
+        .too{
+            width: 250px;
+            float: right;
+            margin-right:30px;
+            margin-top:55px;
+        }
+        .section h3{
+            text-align: center;
+            color: #00b050;
+            font-size: 14px;
+        }
+        .section h3 span{
+            font-size: 10px;
+        }
+        .heat_info th, .heat_info td{
+            border:solid 1px #000;
+        }
+        .heat_info{
+            border-collapse: collapse;
+            font-size:13.5px;
+            margin-top: 10px;
+            margin-bottom: 20px;
+        }
+        .last-col{
+            width:80px;
+        }
+        .footer {
+            margin-left: 40px;
+            margin-top: 20px;
+        }
+        .footer h3, .footer h2{
+            text-align: left;
+        }
+        .footer h2{
+            font-size: 18px;
+            margin-top:40px;
+        }
+        .footer p{
+            font-size: 10px;
+            text-align: left;
+            margin-top: 20px;
+        }
+
     </style>
 </head>
 <body>
     <div id="root">
-        <p>
-            <b>ТЕХНИЧЕСКИЕ УСЛОВИЯ «НА ТЕПЛОСНАБЖЕНИЕ»</b>
-        </p>
-        <p>
-            № {{ $apz->commission->apzHeatResponse->doc_number }} от {{ date('d-m-Y', strtotime($apz->commission->apzHeatResponse->created_at)) }}
-        </p>
-        <br/>
-        <p>
-            <b>Наименование объекта</b>: {{ $apz->project_name }}
-        </p>
-        <p>
-            <b>Адрес проектируемого объекта, Район</b>: {{ $apz->project_address }}, {{ $apz->region }}
-        </p>
-        <p>
-            <b>Заказчик</b> (застройшик, инвестор): {{ $apz->customer }}
-        </p>
-        <p>
-            <b>Проектировщик №ГСЛ, категория</b>: {{ $apz->designer }}
-        </p>
-        <p>
-            <b>Заявитель</b>: {{ $apz->applicant }}
-        </p>
-        <p>
-            <b>Адрес и телефон заявителя</b>: {{ $apz->address }}, {{ $apz->phone }}
-        </p>
-        <p>
-            <b>Дата и время заявления</b>: {{ date('d-m-Y', strtotime($apz->created_at)) }}
-        </p>
-        <br />
-        <div class="row">
-            <p>
-                <b>1. Требования заявителя</b>
-            </p>
+
+    <div class="wrap">
+
+        <header>
+            <div class="logo">
+                <img src="../public/images/heat_pdf_logo.png" alt="heat_logo">
+            </div>
+
+            <div class="divider"></div>
+
+            <div class="small_text">
+                <table>
+                    <tr>
+                        <td>050026, Алматы қаласы, Байзақов көшесі, 221,</td>
+                        <td>050026, город Алматы, улица Байзакова, 221,</td>
+                    </tr>
+                    <tr>
+                        <td>СТН 600700574582, БСН 060640007336,</td>
+                        <td>РНН 600700574582, БИН 060640007336,</td>
+                    </tr>
+                    <tr>
+                        <td>тел.: 8(727) 341-07-00, факс: 8(727) 378-06-73</td>
+                        <td>тел.: 8(727) 341-07-00, факс: 8(727) 378-06-73</td>
+                    </tr>
+
+                </table>
+
+            </div>
+
+            <div class="number">
+                <p><span>_________________</span>№<span>_________________</span></p>
+                <p class="underline">на  № 312&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;от&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;20.11.2018</p>
+                <p class="third">вх.№ 22878&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;от&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 21.11.2018 </p>
+
+            </div>
+
+            <div class="too">
+                <h4>ТОО «Asia Prom Resources»  </h4>
+            </div>
+        </header>
+
+        <div class="section">
+            <h3>ТЕХНИЧЕСКИЕ УСЛОВИЯ<br />
+                на подключение к тепловым сетям …(S<span>от</span>≈     S<span>общ</span>≈  м2),<br />
+                расположенного по адресу: мкр./ул. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;, дом<br />
+                <span style="color: black; font-size: 14px">(кадастровый номер земельного участка ______)</span>
+            </h3>
+            <div>
+                <ol>
+                    <li>Теплоснабжение осуществляется от источников</li>
+                    <li>Точка подключения: ______________. Дополнительные условия и место подключения согласовать с ЦЭР ТОО «АлТС»  (тел. 274-04-47).<br/>
+                        <span style="color: #00b050">- Подключение выполнить по технологии присоединения к предызолированным трубопроводам.<br/>
+                        - <b>Размещение зданий и сооружений Вашего объекта предусмотреть на расстоянии с учетом соблюдения охранной зоны тепловых сетей 2dy_____ мм,
+                        проложенных __________ Вашего объекта. В противном случае выполнить их вынос из-под пятна застройки с переключением существующих потребителей.
+                                Проект выноса тепловых сетей согласовать с ТОО «АлТС».</b></span><br />
+                        Регулирование отпуска тепла: качественное по температурному графику –<sup>о</sup>С.
+                    </li>
+                    <li>Давление теплоносителя в тепловой камере _____:<br/>
+                        <span>- в подающем водоводе 	 _,_ ати<br/>
+                              - в обратном водоводе 	 _,_ ати</span>
+                    </li>
+                    <li>Тепловые сети запроектировать с применением предварительно изолированных трубопроводов с устройством системы оперативного дистанционного контроля.
+                        Способ прокладки тепловых сетей определить проектом с учетом требований МСН 4.02-02-2004 «Тепловые сети».
+                        После выполнения работ комплект исполнительной документации на бумажном носителе и в электронном исполнении, зарегистрированный в                 КГУ «Управление архитектуры и градостроительства г. Алматы», передать в ТОО «АлТС».
+                    </li>
+                    <li>Тепловые нагрузки, Гкал/ч:
+                        <table class="heat_info">
+                            <tr>
+                                <td rowspan="2">Наименование нагрузки</td>
+                                <td rowspan="2">Запрашиваемые</td>
+                                <td rowspan="2">По договору               №           от            г.</td>
+                                <td colspan="2">Прирост</td>
+                            </tr>
+                            <tr>
+                                <td class="last-col">Гкал/ч</td>
+                                <td class="last-col">%</td>
+                            </tr>
+                            <tr>
+                                <td>Отопление</td>
+                                <td>&nbsp;</td>
+                                <td>см.договор</td>
+                                <td class="last-col">???</td>
+                                <td class="last-col">???</td>
+                            </tr>
+                            <tr>
+                                <td>Вентиляция</td>
+                                <td></td>
+                                <td></td>
+                                <td class="last-col">0</td>
+                                <td class="last-col">???</td>
+                            </tr>
+                            <tr>
+                                <td>Горячее водоснаб-жение, макс/ч</td>
+                                <td></td>
+                                <td></td>
+                                <td class="last-col">0</td>
+                                <td class="last-col">???</td>
+                            </tr>
+                            <tr>
+                                <td>Горячее водоснаб-жение, ср/ч</td>
+                                <td></td>
+                                <td></td>
+                                <td class="last-col"></td>
+                                <td class="last-col"></td>
+                            </tr>
+                            <tr>
+                                <th>ИТОГО:</th>
+                                <th>000</th>
+                                <th>000</th>
+                                <th class="last-col">???</th>
+                                <th class="last-col">???</th>
+                            </tr>
+                        </table>
+                    </li>
+                    <li>Окончательные тепловые нагрузки уточнить проектом и согласовать с Оперативно-диспетчерским управлением ТОО «АлТС» (тел.: 378-07-00, вн.1007).
+                        Договор на оказание услуг по передаче и распределению тепловой энергии будет заключен на согласованную уточненную тепловую нагрузку.
+                        В соответствии с п. 12.1 СНиП РК 4.02-42-2006 «Отопление, вентиляция и кондиционирование» в системе вентиляции при обосновании предусмотреть
+                        устройства утилизации теплоты вентиляционных выбросов (рекуперация, рециркуляция вытяжного воздуха) с учетом пунктов 12.2, 12.5 СНиП РК 4.02-42-2006
+                        «Отопление, вентиляция и кондиционирование».
+                    </li>
+                    <li style="color: #00b050">В связи с увеличением циркуляционного расхода выполнить перекладку тепловых сетей от ТК ___  до ТК ___ с увеличением диаметра с 2Dу___ мм на 2Dу___ мм.
+                        Реконструируемые тепловые сети в установленном порядке передать на баланс ТОО «АлТС». <br/>
+                        - Выполнить поверочный расчет диаметров трубопроводов внутриплощадочных тепловых сетей с учетом дополнительно подключаемой нагрузки. В случае необходимости – выполнить их замену.
+
+                    </li>
+                    <li style="color: #00b050">Возврат конденсата не предусмотрен.</li>
+                    <li> На вводе <span style="color: #00b050"> для каждой категории абонентов</span> установить <span style="color: #00b050">прибор</span> учета тепловой энергии, оборудованный модемной связью.
+                        <span style="color: #00b050">Системы отопления и горячего водоснабжения каждой квартиры оборудовать индивидуальными приборами учета расхода теплоты и горячей воды с возможностью
+                            дистанционного снятия показаний.</span> Проект на установку системы учета, схему организации учета,
+                        место установки приборов учета  согласовать со Службой контроля приборов учета тепловой энергии ТОО «АлТС» (тел.: 341-07-77, вн. 2140).</li>
+                    <li style="color: #00b050">Система теплоснабжения:<br />
+                        a)открытая. Предусмотреть догрев ГВС в межотопительный период.<br/>
+                        б)закрытая.<br/>
+                        <span style="color: #000;"> Предусмотреть тепловую изоляцию разводящих трубопроводов и стояков системы горячего водоснабжения.</span>
+                        При присоединении распределительной гребенки индивидуальных потребителей к  стояку общедомовой системы горячего водоснабжения предусмотреть установку обратного клапана.
+                    </li>
+                    <li style="color: #00b050">Подключение выполнить через<br/>
+                        а) узел управления с автоматическим регулированием теплопотребления по зависимой схеме. В случае применения в системе отопления трубопроводов из полимерных материалов – проектирование
+                        вести с учетом требований п. 7.1.3 СНиП РК 4.02-42-2006 «Отопление, вентиляция и кондиционирование».<br/>
+                        б) узел управления с автоматическим регулированием теплопотребления по независимой схеме.<br/>
+                        <b style="color: #000;">По завершении монтажа узла управления выполнить пуско-наладочные работы по автоматизации теплового пункта.</b>
+                    </li>
+                    <li>После предварительного согласования с ЦЭР ТОО «АлТС» проектную документацию (чертежи марки ОВ, ТС, сводный план инженерных сетей) согласовать с
+                        Техническим отделом ТОО «АлТС» (тел.: 378-07-00, вн. 1023). Согласованный проект на бумажном и электронном носителях предоставить в ТОО «АлТС».
+                    </li>
+                    <li><b>Срок действия технических условий:</b> нормативный период проектирования и строительства, предусмотренный в проектно-сметной документации.</li>
+                    <li></li>
+                </ol>
+            </div>
+            <div class="footer">
+                <h3 style="text-align:left;float: left;width: 350px;color: #000;">Главный инженер</h3>
+                <h3  style="color: #000;">Д. Кирдяйкин</h3>
+                <h2 style="color: red">Долг перед ТОО «АлТС»:</h2>
+                <p>Исп. _________________<br/>
+                    тел.: 378-07-00 вн. _____
+                </p>
+            </div>
+
         </div>
-
-        <table width="100%">
-            <tr>
-                <td width="50%"><p>1. Общая тепловая нагрузка</p></td>
-                <td>{{ $apz->apzHeat->general }} Гкал/ч</td>
-            </tr>
-
-            <tr>
-                <td><p>2. Отопление</p></td>
-                <td><p>{{ $apz->apzHeat->main }} Гкал/ч</p></td>
-            </tr>
-
-            <tr>
-                <td><p>3. Вентиляция</p></td>
-                <td><p>{{ $apz->apzHeat->ventilation }} Гкал/ч</p></td>
-            </tr>
-
-            <tr>
-                <td><p>4. Горячее водоснабжение</p></td>
-                <td><p>{{ $apz->apzHeat->water }} Гкал/ч</p></td>
-            </tr>
-
-            <tr>
-                <td><p>5. Технологические нужды(пар)</p></td>
-                <td><p>{{ $apz->apzHeat->tech }} Т/ч</p></td>
-            </tr>
-
-            <tr>
-                <td><p>6. Разделить нагрузку по жилью и по встроенным помещениям</p></td>
-                <td><p>{{ $apz->apzHeat->distribution }} м3 в час</p></td>
-            </tr>
-        </table>
-
-        <div class="row">
-            <p>
-                <b>2. Требования по техническим условиям</b>
-            </p>
-        </div>
-
-        <table width="100%">
-            <tr>
-                <td width="50%"><p>1. Теплоснабжение осуществляется от источников</p></td>
-                <td><p>{{ $apz->commission->apzHeatResponse->resource }}</p></td>
-            </tr>
-
-            <tr>
-                <td><p>2. Точка подключения</p></td>
-                <td><p>{{ $apz->commission->apzHeatResponse->connection_point }}</p></td>
-            </tr>
-
-            <tr>
-                <td><p>3. Давление теплоносителя в тепловой камере {{ $apz->commission->apzHeatResponse->connection_point }}</p></td>
-                <td><p>{{ $apz->commission->apzHeatResponse->trans_pressure }}</p></td>
-            </tr>
-            <tr>
-                <td><p>4. Отопление</p></td>
-                <td>{{ $apz->commission->apzHeatResponse->main_in_contract }} Гкал/ч</td>
-            </tr>
-
-            <tr>
-                <td><p>5. Вентиляция</p></td>
-                <td><p>{{ $apz->commission->apzHeatResponse->ven_in_contract }} Гкал/ч</p></td>
-            </tr>
-
-            <tr>
-                <td><p>6. Горячее водоснабжение</p></td>
-                <td><p>{{ $apz->commission->apzHeatResponse->water_in_contract }} Гкал/ч</p></td>
-            </tr>
-
-            <tr>
-                <td><p>7. Дополнительное</p></td>
-                <td>{{ $apz->commission->apzHeatResponse->addition }}</td>
-            </tr>
-
-        </table>
-
-        <br/>
-        <p>Технические условия (ТУ) действуют в течение всего срока нормативной продолжительности строительства, утвержденной в составе проектной (проектно-сметной) документации</p>
-        <p style="text-align: center;">
-            <barcode code="{{ implode(' ', [$apz->commission->apzHeatResponse->user->last_name, $apz->commission->apzHeatResponse->user->first_name, $apz->commission->apzHeatResponse->user->middle_name]) }}" type="QR" class="barcode" size="1" error="M" />
-        </p>
-        <p>
-            <b>город Алматы 2017 год</b>
-        </p>
     </div>
+    </div>
+    <p style="text-align: center;">
+        <barcode code="{{ implode(' ', [$apz->commission->apzHeatResponse->user->last_name, $apz->commission->apzHeatResponse->user->first_name, $apz->commission->apzHeatResponse->user->middle_name]) }}" type="QR" class="barcode" size="1" error="M" />
+    </p>
 </body>
 </html>
--- resources/views/pdf_templates/tc_phone.blade.php
+++ resources/views/pdf_templates/tc_phone.blade.php
@@ -8,7 +8,10 @@
     <title></title>
 
     <style type="text/css">
-        p, table {text-align: center}
+        p {text-align: center;}
+        .inform p{
+            text-align: left;
+        }
         .row
         {
             display: flex;
@@ -16,6 +19,15 @@
             margin-right: 10px;
             margin-left: 30px;
         }
+        .logo{
+            text-align: center;
+            margin-bottom: 50px;
+        }
+        img{
+            height: 130px;
+        }
+
+
         .col-6
         {
             flex: 0 0 50%;
@@ -29,13 +41,17 @@
 </head>
 <body>
     <div id="root">
+        <div class="logo">
+            <img src="../public/images/Almatytelecom.jpg" alt="telephone_logo">
+        </div>
         <p>
             <b>ТЕХНИЧЕСКИЕ УСЛОВИЯ «НА ТЕЛЕФОНИЗАЦИЮ»</b>
         </p>
         <p>
-            № {{ $apz->commission->apzPhoneResponse->doc_number }} от {{ date('d-m-Y', strtotime($apz->commission->apzPhoneResponse->created_at)) }}
+            {{--№ {{ $apz->commission->apzPhoneResponse->doc_number }} от {{ date('d-m-Y', strtotime($apz->commission->apzPhoneResponse->created_at)) }}--}}
         </p>
         <br/>
+        <div class="inform">
         <p>
             <b>Наименование объекта</b>: {{ $apz->project_name }}
         </p>
@@ -57,6 +73,7 @@
         <p>
             <b>Дата и время заявления</b>: {{ date('d-m-Y', strtotime($apz->created_at)) }}
         </p>
+        </div>
         <br />
         <div class="row">
             <p>
@@ -66,8 +83,8 @@
 
         <table width="100%">
             <tr>
-                <td width="50%"><p>1. Количество ОТА и услуг в разбивке физ.лиц и юр.лиц</p></td>
-                <td><p>{{ $apz->apzPhone->service_num }}</p></td>
+                <td width="80%"><p>1. Количество ОТА и услуг в разбивке физ.лиц и юр.лиц</p></td>
+                {{--<td><p>{{ $apz->apzPhone->service_num }}</p></td>--}}
             </tr>
 
             <tr>
@@ -95,29 +112,29 @@
         <table width="100%">
             <tr>
                 <td width="50%"><p>1. Количество ОТА и услуг в разбивке физ.лиц и юр.лиц</p></td>
-                <td><p>{{ $apz->commission->apzPhoneResponse->service_num }}</p></td>
+                {{--<td><p>{{ $apz->commission->apzPhoneResponse->service_num }}</p></td>--}}
             </tr>
 
             <tr>
                 <td><p>2. Телефонная емкость</p></td>
-                <td><p>{{ $apz->commission->apzPhoneResponse->capacity }}</p></td>
+                {{--<td><p>{{ $apz->commission->apzPhoneResponse->capacity }}</p></td>--}}
             </tr>
 
             <tr>
                 <td><p>3. Планируемая телефонная канализация</p></td>
-                <td><p>{{ $apz->commission->apzPhoneResponse->sewage }}</p></td>
+                {{--<td><p>{{ $apz->commission->apzPhoneResponse->sewage }}</p></td>--}}
             </tr>
 
             <tr>
                 <td><p>4. Пожелания заказчика (тип оборудования, тип кабеля и др.)</p></td>
-                <td>{{ $apz->commission->apzPhoneResponse->client_wishes }}</td>
+                {{--<td>{{ $apz->commission->apzPhoneResponse->client_wishes }}</td>--}}
             </tr>
         </table>
 
         <br/>
         <p>Технические условия (ТУ) действуют в течение всего срока нормативной продолжительности строительства, утвержденной в составе проектной (проектно-сметной) документации</p>
         <p style="text-align: center;">
-            <barcode code="{{ implode(' ', [$apz->commission->apzPhoneResponse->user->last_name, $apz->commission->apzPhoneResponse->user->first_name, $apz->commission->apzPhoneResponse->user->middle_name]) }}" type="QR" class="barcode" size="1" error="M" />
+            {{--<barcode code="{{ implode(' ', [$apz->commission->apzPhoneResponse->user->last_name, $apz->commission->apzPhoneResponse->user->first_name, $apz->commission->apzPhoneResponse->user->middle_name]) }}" type="QR" class="barcode" size="1" error="M" />--}}
         </p>
         <p>
             <b>город Алматы 2017 год</b>
