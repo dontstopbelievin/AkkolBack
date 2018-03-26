18f89bbfb3324b904962b5be2a7d83f4d8ff9be5
Zhalgas <1994-23@mail.ru>
2018-03-26 18:53:47 +0600

    Настроил cors для браузера firefox

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,10 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Headers', '*')
+            ->header('Access-Control-Allow-Origin', \Request::server('HTTP_ORIGIN'))
+            ->header('Access-Control-Allow-Headers', 'origin, x-requested-with, content-Type, accept, authorization')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Max-Age', '86400')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
